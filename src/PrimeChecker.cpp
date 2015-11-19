01ff9cc55249cb0b18407bbb3ed2673903290aba
skylasercolorado <JoeRohd@gmail.com>
2015-11-19 07:23:07 -0800

    refactoring IsPrime(), part 3

--- src/PrimeChecker.cpp
+++ src/PrimeChecker.cpp
@@ -10,33 +10,35 @@ bool PrimeChecker::IsPrime(int n)
 
   for(int i = 2; i < n; i++)
     {
-      if((n % i) == 0)
+      if((n % i) == 0) // For exact division there is no reaminder. Then, this is a factor.
 	{
 	  factors.push_back(i);
 
 	  if(_verbose)
-	    //cout << "\n\t" << n << " / " << i << " = " << n/i << ". Residue: " << n%i << " ---> Number " << i << " IS A FACTOR !!! :)";
 	    calculationMessage(n, i, " IS A FACTOR !!! :)");
 	}
       else
 	if(_verbose)
-	  //cout << "\n\t" << n << " / " << i << " = " << n/i << ". Residue: " << n%i << " ---> Number " << i << " IS NOT A FACTOR !!! :(";
 	  calculationMessage(n, i, " IS NOT A FACTOR !!! :)");
     }
 
-  cout << "\n\tFactors (other than 1 and " << n << "): ";
-  for(vector<int>::iterator it = factors.begin(); it != factors.end(); it++)
+  if(_verbose)
     {
-      cout << *it << ", ";
+      cout << "\n\tFactors (other than 1 and " << n << "): ";
+      for(vector<int>::iterator it = factors.begin(); it != factors.end(); it++)
+	{
+	  cout << *it << ", ";
+	}
+
+      if(factors.size() == 0) cout << "NONE";
     }
 
   if(factors.size() == 0)
     {
-      cout << "NONE";
       cout << "\n\n\tNumber " << n << " IS PRIME!!! :)\n\n";
       return true;
     }
-  else
+  else 
     {
       cout << "\n\n\tNumber " << n << " IS COMPOSITE!!! :(\n\n";
       return false;
